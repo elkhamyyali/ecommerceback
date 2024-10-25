c427587d085a332b7100c8cf530a01f08269bbe0
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-10-25 21:40:48 +0300

    commit

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const autoIncrement = require("mongoose-sequence");
+const autoIncrement = require("mongoose-sequence")(mongoose);
 
 // npm install --save --legacy-peer-deps mongoose-auto-increment
 const connection = mongoose.createConnection(process.env.DB_URI);
